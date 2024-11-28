fd221436aac98fb4713f03e4030c662042b1154d
Evan Robertson <evan-robertson@outlook.com>
2024-11-28 13:54:48 -0500

    Updated in-game color guide to change based on preferred colors

--- Chisel/Assets/Scripts/UI/ColourGuideToggle.cs
+++ Chisel/Assets/Scripts/UI/ColourGuideToggle.cs
@@ -2,9 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class ColourGuideToggle : MonoBehaviour
 {
     public GameObject colourGuidePanel; // Reference to the guide panel
+
+    [SerializeField] private List<SpriteRenderer> images;
 
     private bool isGuideVisible = false;
 
@@ -12,5 +15,25 @@ public class ColourGuideToggle : MonoBehaviour
     {
         isGuideVisible = !isGuideVisible; // Toggle the visibility state
         colourGuidePanel.SetActive(isGuideVisible); // Show or hide the panel
+
+        SetupColors(); // Setup preferred colors
+    }
+
+    void SetupColors()
+    {
+        List<Color> colors;
+        if (SettingsManager.Instance.GetColors().Count != 0)
+        {
+           colors = new List<Color>(SettingsManager.Instance.GetColors());
+        }
+        else
+        {
+            colors = new List<Color>(SettingsManager.Instance.GetDefaultColors());
+        }
+
+        for (int i = 0; i < images.Count; i++)
+        {
+            images[i].color = colors[i];
+        }
     }
 }
